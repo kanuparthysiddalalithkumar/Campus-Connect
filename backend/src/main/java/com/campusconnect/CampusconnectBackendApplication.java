97df7e31522ed950c392e0297be040ca84c0d5e4
Sidda lalith kumar <kanuparthylalithkumarreddy@gmail.com>
2026-04-08 10:18:18 +0530

    Final CORS fix

--- backend/src/main/java/com/campusconnect/CampusconnectBackendApplication.java
+++ backend/src/main/java/com/campusconnect/CampusconnectBackendApplication.java
@@ -3,9 +3,26 @@ package com.campusconnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class CampusconnectBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CampusconnectBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+		};
 	}
 }
